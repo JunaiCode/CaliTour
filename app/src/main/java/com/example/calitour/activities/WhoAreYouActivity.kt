b12f7028e4346ddcd2218073038c52e7310fbfb2
juanosorio0219 <juanjo9464@gmail.com>
2023-11-13 10:55:49 -0500

    Screens added

---
+++ app/src/main/java/com/example/calitour/activities/WhoAreYouActivity.kt
@@ -0,0 +1,12 @@
+package com.example.calitour.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calitour.R
+
+class WhoAreYouActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_who_are_you)
+    }
+}
