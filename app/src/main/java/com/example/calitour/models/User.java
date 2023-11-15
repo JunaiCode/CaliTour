3f65bb091fac3fa8f8e9d17479b3a7e24e701056
juanosorio0219 <juanjo9464@gmail.com>
2023-11-14 20:43:23 -0500

    Register and alerts working

--- app/src/main/java/com/example/calitour/models/User.java
+++ app/src/main/java/com/example/calitour/models/User.java
@@ -1,21 +1,34 @@
 package com.example.calitour.models;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 public class User implements Serializable {
 
+    private String id;
     private String name;
     private String email;
     private String birthday;
-
     private String phoneNumber;
+    private String points;
 
+    public User(){}
 
-    public User(String name, String email, String birthday, String phoneNumber) {
+    public User(String id, String name, String email, String birthday, String phoneNumber, String points) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.birthday = birthday;
         this.phoneNumber = phoneNumber;
+        this.points = points;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -48,5 +61,13 @@ public class User implements Serializable {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
     }
 }
