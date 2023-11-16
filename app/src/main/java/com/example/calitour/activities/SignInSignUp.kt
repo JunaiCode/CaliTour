a222f6a04f5f4f60451f7a7e1d6cd659865292f5
Danilo Erazo <erazodanilo1218@gmail.com>
2023-11-16 01:00:22 -0500

    feat: added fragments

---
+++ app/src/main/java/com/example/calitour/activities/SignInSignUp.kt
@@ -0,0 +1,30 @@
+package com.example.calitour.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.calitour.R
+import com.example.calitour.activities.fragments.SignInFragment
+import com.example.calitour.activities.fragments.SignUpFragment
+import com.example.calitour.databinding.ActivitySignInsignUpBinding
+
+class SignInSignUp : AppCompatActivity() {
+
+    private val binding:ActivitySignInsignUpBinding by lazy {
+        ActivitySignInsignUpBinding.inflate(layoutInflater)
+    }
+
+    private val signin = SignInFragment()
+    private val signup = SignUpFragment()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        showFragment(signin)
+        setContentView(binding.root)
+    }
+
+    fun showFragment(fragment: Fragment){
+
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, fragment).commit()
+
+    }
+}
