882c327fb6128182eecf51d4e59b910a8a2b7ec1
Juan Fernando Martinez <jm1811324@gmail.com>
2023-11-16 19:38:54 -0500

    profile picture load finished

--- app/src/main/java/com/example/calitour/activities/fragments/UserProfileFragment.kt
+++ app/src/main/java/com/example/calitour/activities/fragments/UserProfileFragment.kt
@@ -1,12 +1,10 @@
 package com.example.calitour.activities.fragments
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
@@ -16,7 +14,6 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import java.io.File
-import java.io.IOException
 
 class UserProfileFragment: Fragment() {
 
@@ -29,20 +26,10 @@ class UserProfileFragment: Fragment() {
         val binding = UserProfileFragmentBinding.inflate(inflater, container, false)
 
         viewModel._user.observe(viewLifecycleOwner){
+            Glide.with(this@UserProfileFragment)
+                .load("https://firebasestorage.googleapis.com/v0/b/calitour.appspot.com/o/profileImages%2Fbde9a612-0bc6-4cb2-8396-fd37d899050d?alt=media&token=7207e3a5-8514-4831-87a7-58e100b28efb")
+                .into(binding.userIV)
 
-            val storageReference = Firebase.storage.getReference("/profileImages/${it.photoUri}.jpeg")
-
-            try {
-                val localFile = File.createTempFile("user_picture", ".jpeg")
-                storageReference.getFile(localFile).addOnSuccessListener {
-                    val bitmap : Bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
-                    binding.userIV.setImageBitmap(bitmap)
-                }.addOnFailureListener{
-                    Toast.makeText(requireContext(), "No fué posible recuperar la imagen", Toast.LENGTH_LONG)
-                }
-            }catch (e: IOException){
-                println("Haha caiste!!")
-            }
 
 
         }
