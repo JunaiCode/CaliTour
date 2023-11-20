ad484dea5d98977be30fabfeb1bce46f77a53cd1
JunaiCode <juanpalta24@hotmail.com>
2023-11-20 18:40:13 -0500

    feature: add fragments to eventActivity

--- app/src/main/java/com/example/calitour/activities/CreateEventActivity.kt
+++ app/src/main/java/com/example/calitour/activities/CreateEventActivity.kt
@@ -2,10 +2,16 @@ package com.example.calitour.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.calitour.R
+import com.example.calitour.activities.fragments.CreateEventFragment
+import com.example.calitour.activities.fragments.CreateProductFragment
 import com.example.calitour.databinding.ActivityCreateEventBinding
 
 class CreateEventActivity : AppCompatActivity() {
+
+    private val createEventFragment = CreateEventFragment.newInstance()
+    private val createProductFragment = CreateProductFragment.newInstance()
 
     private val binding: ActivityCreateEventBinding by lazy {
         ActivityCreateEventBinding.inflate(layoutInflater)
@@ -13,5 +19,9 @@ class CreateEventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    fun showFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer,fragment).commit()
     }
 }
