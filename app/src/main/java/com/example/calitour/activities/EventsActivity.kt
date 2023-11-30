a1b81349b7826b4d3eb126caa56fb59bda2e54dc
juanosorio0219 <juanjo9464@gmail.com>
2023-11-29 21:25:19 -0500

    Screen added

---
+++ app/src/main/java/com/example/calitour/activities/EventsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.calitour.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calitour.R
+
+class EventsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_events)
+    }
+}
