c52f891113f67a59f59bed4024f0094aafc47829
JunaiCode <juanpalta24@hotmail.com>
2023-11-30 14:28:10 -0500

    progress in visualize activeEvents

--- app/src/main/java/com/example/calitour/activities/fragments/ActiveEventFragment.kt
+++ app/src/main/java/com/example/calitour/activities/fragments/ActiveEventFragment.kt
@@ -14,9 +14,12 @@ import com.example.calitour.databinding.ActivityCreateEventBinding
 import com.example.calitour.databinding.CreateEventFragmentBinding
 import com.example.calitour.databinding.WelcomeFragmentBinding
 import com.example.calitour.model.DTO.EventDocumentDTO
+import com.example.calitour.viewmodel.EntityViewModel
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ActiveEventFragment: Fragment() {
-
+    private val vm = EntityViewModel()
     private lateinit var  binding: ActiveEventFragmentBinding
     private lateinit var  adapter: ActiveEventAdapter
 
@@ -27,11 +30,13 @@ class ActiveEventFragment: Fragment() {
     ): View? {
         binding = ActiveEventFragmentBinding.inflate(inflater, container, false)
         adapter = ActiveEventAdapter()
+        vm.getEventsAvailablesByEntityId(Firebase.auth.currentUser?.uid.toString())
         binding.activeEventList.adapter = adapter
-        Log.e("Si",arguments.toString())
         binding.activeEventList.layoutManager = LinearLayoutManager(context)
         binding.activeEventList.setHasFixedSize(true)
-
+        vm.eventsQuery.observe(viewLifecycleOwner){ list->
+            adapter.setList(list)
+        }
         return binding.root
     }
     companion object{
