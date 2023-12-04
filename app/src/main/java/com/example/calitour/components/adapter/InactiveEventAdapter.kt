ba17c68fc091082e4f7c5e30d133da6a0ec58c2f
JunaiCode <juanpalta24@hotmail.com>
2023-12-04 13:19:49 -0500

    edit design of activeEventAdapter, fix bug in select image

--- app/src/main/java/com/example/calitour/components/adapter/InactiveEventAdapter.kt
+++ app/src/main/java/com/example/calitour/components/adapter/InactiveEventAdapter.kt
@@ -10,10 +10,13 @@ import com.bumptech.glide.Glide
 import com.example.calitour.R
 import com.example.calitour.components.views.InactiveEventViewHolder
 import com.example.calitour.model.DTO.EventDocumentDTO
+import com.example.calitour.viewmodel.CreateEventProductViewModel
+import java.text.SimpleDateFormat
+
 class InactiveEventAdapter: Adapter<InactiveEventViewHolder>() {
     private var inactiveEvents : ArrayList<EventDocumentDTO> = arrayListOf()
     private var uris :ArrayList<Uri> = arrayListOf()
-
+    private val vm: CreateEventProductViewModel = CreateEventProductViewModel()
     init {
 
     }
@@ -28,10 +31,19 @@ class InactiveEventAdapter: Adapter<InactiveEventViewHolder>() {
 
 
     override fun onBindViewHolder(holder: InactiveEventViewHolder, position: Int) {
-        holder.date.text = inactiveEvents[position].date.toString()
+        val sdf = SimpleDateFormat("dd/MM - HH:mm a")
+        val date = sdf.format(inactiveEvents[position].date.toDate())
+        holder.date.text = date
         holder.place.text = inactiveEvents[position].place
-        holder.price.text = "Multiples precios"
-        holder.title.text = inactiveEvents[position].name
+        val price = vm.getPricesEvent(inactiveEvents[position].id).value?.get(0)?.fee?.toInt();
+        if(price != 0){
+            holder.price.text = price.toString()
+        }else{
+            holder.price.text = R.string.free.toString()
+        }
+        if(uris.size>0){
+            Glide.with(holder.itemView.context).load(uris[position]).into(holder.img)
+        }
         if(uris.size>0){
             Glide.with(holder.itemView.context).load(uris[position]).into(holder.img)
         }
